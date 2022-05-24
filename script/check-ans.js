c77d2db22de63f6ec7cd14eeaa711a24cb1a095a
BoszGTec <95701554+BoszGTec@users.noreply.github.com>
2022-05-25 04:16:27 +0700

    Update check-ans.js

--- script/check-ans.js
+++ script/check-ans.js
@@ -2,13 +2,16 @@ function check_ans(){
 var ans = document.getElementsByClassName("ans")
  for (let i=0 ; i<c_problem ; i++){ 
   if(ans[i].value==(a[i]*b[i])){
-   ans[i].style="background-color : green ;"
+   ans[i].style="background-color : var(--bs-green) ; color : var(--bs-white) ;"
+   ans[i].disable = true
   }
   else if(ans[i].value!=(a[i]*b[i])&&ans[i].value!=""){
-   ans[i].style="background-color : red ;"
+   ans[i].style="background-color : var(--bs-red) ; color : var(--bs-white) ;"
+   ans[i].disable = true
   }
   else {
-   ans[i].style="background-color : yellow ;"
+   ans[i].style="background-color : var(--bs-yellow) ;"
+   ans[i].disable = true
   }
  }
 }
