c826c0102fa1c26488ff83c6a045ca61fdddd5d1
BoszGTec <95701554+BoszGTec@users.noreply.github.com>
2022-05-25 04:45:04 +0700

    Update check-ans.js

--- script/check-ans.js
+++ script/check-ans.js
@@ -2,16 +2,24 @@ function check_ans(){
 var ans = document.getElementsByClassName("ans")
  for (let i=0 ; i<c_problem ; i++){ 
   if(ans[i].value==(a[i]*b[i])){
-   ans[i].style="background-color : var(--bs-green) ; color : var(--bs-white) ;"
-   ans[i].disable = true
+   ans[i].style = "background-color : var(--bs-green) ; color : var(--bs-white) ;"
+   ans[i].readonly = true
   }
   else if(ans[i].value!=(a[i]*b[i])&&ans[i].value!=""){
-   ans[i].style="background-color : var(--bs-red) ; color : var(--bs-white) ;"
-   ans[i].disable = true
+   ans[i].style = "background-color : var(--bs-red) ; color : var(--bs-white) ;"
+   ans[i].readonly = true
   }
   else {
-   ans[i].style="background-color : var(--bs-yellow) ;"
-   ans[i].disable = true
+   ans[i].style = "background-color : var(--bs-yellow) ;"
+   ans[i].readonly = true
   }
+ }
+}
+
+function fix_ans(){
+ var ans = document.getElementsByClassName("ans")
+ for (let i=0 ; i<c_problem ; i++){
+  ans[i].style = ""
+  ans[i].readonly = false
  }
 }
