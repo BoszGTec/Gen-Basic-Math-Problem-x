c2723d05d70654440006d4bb740045a0380af300
BoszGTec <95701554+BoszGTec@users.noreply.github.com>
2022-05-25 03:50:33 +0700

    Update creatproblem.js

--- script/creatproblem.js
+++ script/creatproblem.js
@@ -4,9 +4,9 @@ for (let i=0 ; i<c_problem ; i++){
   a[i]=Math.floor(Math.random() * 100000);
   b[i]=Math.floor(Math.random() * 100000);
 }
-var elem = '<div class="d-flex" >'
+var elem = '<div>'
 for (let i=0 ; i<c_problem ; i++){
-  elem += "<div>"
+  elem += '<div class="d-flex">'
   elem += "<span>"
   elem += a[0]+"×"+b[0]+"="
   elem += '<input type="number" class="form-control ans" />'
