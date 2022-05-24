6afd39e7eb55a3c14dbc3b8df3cf1c3883d57411
BoszGTec <95701554+BoszGTec@users.noreply.github.com>
2022-05-25 03:45:03 +0700

    Create check-ans.js

---
+++ script/check-ans.js
@@ -0,0 +1,14 @@
+function check_ans(){
+var ans = document.getElementsByClass("ans")
+ for (let i=0 ; i<c_problem ; i++){ 
+  if(ans[i].value==(a[i]*b[i])){
+   ans[i].style="background-color : green ;"
+  }
+  else if(ans[i].value!=(a[i]*b[i])){
+   ans[i].style="background-color : red ;"
+  }
+  else {
+   ans[i].style="background-color : yellow ;"
+  }
+ }
+}
