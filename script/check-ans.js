5d6d3d152bb2457a8ac9241118c1cd0ea6b8d674
BoszGTec <95701554+BoszGTec@users.noreply.github.com>
2022-05-25 03:55:40 +0700

    Update check-ans.js

--- script/check-ans.js
+++ script/check-ans.js
@@ -1,5 +1,5 @@
 function check_ans(){
-var ans = document.getElementsByClass("ans")
+var ans = document.getElementsByClassName("ans")
  for (let i=0 ; i<c_problem ; i++){ 
   if(ans[i].value==(a[i]*b[i])){
    ans[i].style="background-color : green ;"
