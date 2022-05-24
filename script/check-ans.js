f4f400dc835c5198dfedd8726244cf23f0250099
BoszGTec <95701554+BoszGTec@users.noreply.github.com>
2022-05-25 05:02:14 +0700

    Update check-ans.js

--- script/check-ans.js
+++ script/check-ans.js
@@ -1,17 +1,22 @@
 function check_ans(){
 var ans = document.getElementsByClassName("ans")
+var check_word = document.getElementsByClassName("check_word")
  for (let i=0 ; i<c_problem ; i++){ 
+  ans[i].readonly = true
   if(ans[i].value==(a[i]*b[i])){
    ans[i].style = "background-color : var(--bs-green) ; color : var(--bs-white) ;"
-   ans[i].readonly = true
+   check_word[i].innerText = "ถูก"
+   check_word[i].style = "color : var(--bs-green) ;"
   }
   else if(ans[i].value!=(a[i]*b[i])&&ans[i].value!=""){
    ans[i].style = "background-color : var(--bs-red) ; color : var(--bs-white) ;"
-   ans[i].readonly = true
+   check_word[i].innerText = "ผิด"
+   check_word[i].style = "color : var(--bs-red) ; "
   }
   else {
    ans[i].style = "background-color : var(--bs-yellow) ;"
-   ans[i].readonly = true
+   check_word[i].innerText = "ไม่ได้ตอบ"
+   check_word[i].style = "color : var(--bs-yellow) ; "
   }
  }
 }
@@ -20,6 +25,6 @@ function fix_ans(){
  var ans = document.getElementsByClassName("ans")
  for (let i=0 ; i<c_problem ; i++){
   ans[i].style = ""
-  ans[i].readonly = false
+  ans[i].readonly = False
  }
 }
