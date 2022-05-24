80f7593bd833c8d442163b9e2dc8ec54701759f6
BoszGTec <95701554+BoszGTec@users.noreply.github.com>
2022-05-25 03:35:36 +0700

    Create creatproblem.js

---
+++ script/creatproblem.js
@@ -0,0 +1,16 @@
+let c_problem=10
+let a=[] , b=[]
+for (let i=0 ; i<c_problem ; i++){
+  a[i]=Math.floor(Math.random() * 100000);
+  b[i]=Math.floor(Math.random() * 100000);
+}
+var elem = '<div class="d-flex" >'
+for (let i=0 ; i<c_problem ; i++){
+  elem += "<div>"
+  elem += "<span>"
+  elem += a[0]+"×"+b[0]+"="
+  elem += '<input type="number" class="form-control ans" />'
+  elem += "</div>"
+}
+elem = '</div>'
+document.getElementById("content-mathproblem").innerHTML=elem
