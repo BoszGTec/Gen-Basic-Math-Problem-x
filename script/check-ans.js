8deab5ceb848b849e03e70f153cfd8698456ebba
BoszGTec <95701554+BoszGTec@users.noreply.github.com>
2022-05-25 20:23:13 +0700

    Update check-ans.js

--- script/check-ans.js
+++ script/check-ans.js
@@ -23,9 +23,22 @@ var check_word = document.getElementsByClassName("check_word")
 
 function fix_ans(){
  var ans = document.getElementsByClassName("ans")
+ if(ans[9].value==6){ document.getElementById("view_ans").hidden = false }
+ 
  for (let i=0 ; i<c_problem ; i++){
   ans[i].value = ""
   ans[i].style = ""
   ans[i].disabled = false
  }
+}
+
+function view_ans(){
+  check_ans()
+  var cw=document.getElementsByClassName("check_word")
+  var sp=document.createElement("span")
+  for (let i=0 ; i<c_problem ; i++){
+   sp=document.createElement("span")
+   sp.innerText += " "+a[i]*b[i]
+   sp.style = "color : var(--bs-blue);"
+   cw[i].appendChild(sp)
 }
