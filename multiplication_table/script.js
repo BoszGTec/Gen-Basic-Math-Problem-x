32f37c90471655d37a650aedc67872cca670ddd1
BoszGTec <95701554+BoszGTec@users.noreply.github.com>
2022-05-27 11:15:21 +0700

    Update script.js

--- multiplication_table/script.js
+++ multiplication_table/script.js
@@ -9,6 +9,6 @@ document.getElementsByTagName("body")[0].innerHTML = '  <h1>Gen Basic Math Probl
   <hr/><center>©2022 BoszGTec</center> '
 
 
-document.getElementsByTagName("html")[0].innerHTML += ' \
-  <script src="../script/creatproblem-multiplication_table.js"></script>  \
-  <script src="../script/check-ans.js"></script> '
+document.getElementsByTagName("body")[0].innerHTML += ' \
+  <script src="https://raw.githubusercontent.com/BoszGTec/Gen-Basic-Math-Problem-x/main/script/creatproblem-multiplication_table.js"></script>  \
+  <script src="https://raw.githubusercontent.com/BoszGTec/Gen-Basic-Math-Problem-x/main/script/check-ans.js"></script> '
