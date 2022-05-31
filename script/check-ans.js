1b0a919c9f30d7f333dd52c1aa9798837d7c83a8
BoszGTec <95701554+BoszGTec@users.noreply.github.com>
2022-05-31 20:56:49 +0700

    Update check-ans.js

--- script/check-ans.js
+++ script/check-ans.js
@@ -1,19 +1,53 @@
+const Toast = Swal.mixin({
+  toast: true , position: 'top-end',
+  showConfirmButton: false , timer: 5000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
+function have_every_ans(){
+  let ans = document.getElementsByClassName("ans")
+  let bool = true
+  for (let i=0 ; i<c_problem ; i++){
+    if(ans[i].value==""){
+      bool = false
+      Toast.fire({icon: 'warning',title: 'ตอบไม่ครบ'})
+      break ;
+    }
+    else{bool = true}
+  }
+  return bool
+}
+
+
 function check_ans(){
+let hea = have_every_ans()
 var ans = document.getElementsByClassName("ans")
 var check_word = document.getElementsByClassName("check_word")
+if (hea){
  for (let i=0 ; i<c_problem ; i++){ 
-  ans[i].disabled = true
-  if(ans[i].value==(a[i]*b[i])){
-   ans[i].style = "background-color : var(--bs-green) ; color : var(--bs-white) ;"
-   check_word[i].innerText = "ถูก"
-   check_word[i].style = "color : var(--bs-green) ;"
+   ans[i].disabled = true
+   if(ans[i].value==(a[i]*b[i])){
+    ans[i].style = "background-color : var(--bs-green) ; color : var(--bs-white) ;"
+    check_word[i].innerText = "ถูก"
+    check_word[i].style = "color : var(--bs-green) ;"
+   }
+   else if(ans[i].value!=(a[i]*b[i])&&ans[i].value!=""){
+    ans[i].style = "background-color : var(--bs-red) ; color : var(--bs-white) ;"
+    check_word[i].innerText = "ผิด"
+    check_word[i].style = "color : var(--bs-red) ; "
+   }
+   else {
+    ans[i].style = "background-color : var(--bs-yellow) ;"
+    check_word[i].innerText = "ไม่ได้ตอบ"
+    check_word[i].style = "color : var(--bs-yellow) ; "
+   }
   }
-  else if(ans[i].value!=(a[i]*b[i])&&ans[i].value!=""){
-   ans[i].style = "background-color : var(--bs-red) ; color : var(--bs-white) ;"
-   check_word[i].innerText = "ผิด"
-   check_word[i].style = "color : var(--bs-red) ; "
-  }
-  else {
+ }
+else { for (let i=0 ; i<c_problem ; i++){
    ans[i].style = "background-color : var(--bs-yellow) ;"
    check_word[i].innerText = "ไม่ได้ตอบ"
    check_word[i].style = "color : var(--bs-yellow) ; "
